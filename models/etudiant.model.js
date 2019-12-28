b389cd7e9f995cc1a463cd106a2a881f3e2b8cef
fatima-bouzidene <58086946+fatima-bz@users.noreply.github.com>
2019-12-28 05:01:21 +0100

    Add files via upload

---
+++ models/etudiant.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const etudiantSchema = new Schema({
+  username: { type: String, required: true },
+  nom: { type: String , required: true},
+  prenom: { type: String , required: true},
+  age: { type: Number, required: true },
+  date: { type: Date, required: true },
+}, {
+  timestamps: true,
+});
+
+const Etudiant = mongoose.model('Etudiant', etudiantSchema);
+
+module.exports = Etudiant;
